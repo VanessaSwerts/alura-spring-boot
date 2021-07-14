21517d668136ec4fa6c3763783885b564687914e
Vanessa Swerts <vanessaswerts@gec.inatel.br>
2021-07-14 14:33:34 -0300

    Adicioanndo configurações de segurança

--- src/main/java/br/com/alura/forum/modelo/Usuario.java
+++ src/main/java/br/com/alura/forum/modelo/Usuario.java
@@ -1,19 +1,32 @@
 package br.com.alura.forum.modelo;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
-public class Usuario {
-	
-	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+public class Usuario implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
 	private String senha;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	private List<Perfil> perfis = new ArrayList<>();
 
 	@Override
 	public int hashCode() {
@@ -39,8 +52,7 @@ public class Usuario {
 			return false;
 		return true;
 	}
-	
-	
+
 	// Getters e Setters
 	public Long getId() {
 		return id;
@@ -72,6 +84,42 @@ public class Usuario {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+	
+	//Métodos da interface UserDetails 
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return this.perfis;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 
 }

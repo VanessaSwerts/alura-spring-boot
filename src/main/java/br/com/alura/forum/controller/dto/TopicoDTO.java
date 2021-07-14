4e08ec7089b4600b5b2e62844a134efa165f1923
Vanessa Swerts <vanessaswerts@gec.inatel.br>
2021-07-14 11:18:27 -0300

    Adicionando sistema de paginação

--- src/main/java/br/com/alura/forum/controller/dto/TopicoDTO.java
+++ src/main/java/br/com/alura/forum/controller/dto/TopicoDTO.java
@@ -1,8 +1,8 @@
 package br.com.alura.forum.controller.dto;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Page;
 
 import br.com.alura.forum.modelo.Topico;
 
@@ -36,9 +36,8 @@ public class TopicoDTO {
 		return dataCriacao;
 	}
 
-	public static List<TopicoDTO> converter(List<Topico> topicos) {
-		// Percorre a lista de topicos, converte para TopicosDTO e armazena em outra lista
-		return topicos.stream().map(TopicoDTO::new).collect(Collectors.toList());
+	public static Page<TopicoDTO> converter(Page<Topico> topicos) {
+		return topicos.map(TopicoDTO::new);
 	}
 
 }

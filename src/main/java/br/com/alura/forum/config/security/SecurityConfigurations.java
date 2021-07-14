844ccbbf21167f7ea17621c38ca8df82515f85f8
Vanessa Swerts <vanessaswerts@gec.inatel.br>
2021-07-14 17:51:42 -0300

    Implementando a autentificação do token

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -19,6 +20,9 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private AutenticacaoService autenticacaoService;
+	
+	@Autowired
+	private TokenService tokenService;
 
 	@Override
 	@Bean
@@ -38,7 +42,8 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers(HttpMethod.GET, "/topicos").permitAll()
 				.antMatchers(HttpMethod.GET, "/topicos/*").permitAll().antMatchers(HttpMethod.POST, "/auth").permitAll()
 				.anyRequest().authenticated().and().csrf().disable().sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				.addFilterBefore(new AutenticacaoViaTokenFilter(tokenService), UsernamePasswordAuthenticationFilter.class);
 	}
 
 	// Configurações de recursos estáticos(JS, CSS, Imagens, etc)
--- src/main/java/br/com/alura/forum/config/security/TokenService.java
+++ src/main/java/br/com/alura/forum/config/security/TokenService.java
@@ -12,27 +12,31 @@ import io.jsonwebtoken.SignatureAlgorithm;
 
 @Service
 public class TokenService {
-	
+
 	@Value("${forum.jwt.expiration}")
 	private String expiration;
-	
-	@Value("${forum.jwt.secret}") 
+
+	@Value("${forum.jwt.secret}")
 	private String secret;
 
 	public String gerarToken(Authentication authetication) {
 
 		Usuario usuarioLogado = (Usuario) authetication.getPrincipal();
 		Date hoje = new Date();
-		Date dataExpiracao = new Date(hoje.getTime() + Long.parseLong(expiration)) ;
+		Date dataExpiracao = new Date(hoje.getTime() + Long.parseLong(expiration));
 
-		return Jwts.builder()
-				.setIssuer("API do Fórum da Alura")
-				.setSubject(usuarioLogado.getId().toString())
-				.setIssuedAt(hoje)
-				.setExpiration(dataExpiracao)
-				.signWith(SignatureAlgorithm.HS256, secret)
-				.compact();
+		return Jwts.builder().setIssuer("API do Fórum da Alura").setSubject(usuarioLogado.getId().toString())
+				.setIssuedAt(hoje).setExpiration(dataExpiracao).signWith(SignatureAlgorithm.HS256, secret).compact();
 
+	}
+
+	public boolean isTokenValido(String token) {
+		try {
+			Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
 	}
 
 }

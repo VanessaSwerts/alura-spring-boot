4b3713fa99d53a2a435389e6bf52628adb052742
Vanessa Swerts <vanessaswerts@gec.inatel.br>
2021-07-15 09:35:42 -0300

    Adicionando configs do Swagger

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -58,6 +58,8 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	// Configurações de recursos estáticos(JS, CSS, Imagens, etc)
 	@Override
 	public void configure(WebSecurity web) throws Exception {
+	    web.ignoring()
+	        .antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
 	}
 
 }
